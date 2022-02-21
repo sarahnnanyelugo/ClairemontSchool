4a27a83cb45cc30cc275a9b8e1d721bf7f9359d1
Sarah Nnanyelugo <sarahnnanyelugo@gmail.com>
2022-02-21 21:37:11 +0100

    webnav

--- Assets/js/custom.js
+++ Assets/js/custom.js
@@ -701,4 +701,18 @@ function mapRange(value, fromIn, toIn, fromOut, toOut) {
 
 
 
-
+//for sticky navbar on scroll
+document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+          document.getElementById('navbar').classList.add('fixed-top');
+          // add padding top to show content behind navbar
+          navbar_height = document.querySelector('.navbar').offsetHeight;
+          document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+          document.getElementById('navbar').classList.remove('fixed-top');
+           // remove padding top from body
+          document.body.style.paddingTop = '0';
+        } 
+    });
+  });
