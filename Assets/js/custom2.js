733526ac2ccd553c8705e8308d2516cb744a40bf
Sarah Nnanyelugo <sarahnnanyelugo@gmail.com>
2022-03-11 16:55:49 +0100

    blog

--- Assets/js/custom2.js
+++ Assets/js/custom2.js
@@ -58,3 +58,63 @@ function includeHTML() {
 
   //   })
   // });
+
+
+
+
+
+
+
+  // $( document ).ready(function() {
+  //   $('[data-toggle=search-form]').click(function() {
+  //       $('.search-form-wrapper').toggleClass('open');
+  //       $('.search-form-wrapper .search').focus();
+  //       $('html').toggleClass('search-form-open');
+  //     });
+  //     $('[data-toggle=search-form-close]').click(function() {
+  //       $('.search-form-wrapper').removeClass('open');
+  //       $('html').removeClass('search-form-open');
+  //     });
+  //   $('.search-form-wrapper .search').keypress(function( event ) {
+  //     if($(this).val() == "Search") $(this).val("");
+  //   });
+
+  //   $('.search-close').click(function(event) {
+  //     $('.search-form-wrapper').removeClass('open');
+  //     $('html').removeClass('search-form-open');
+  //   });
+
+
+  //   $('[data-toggle=search-form]').click(function() {
+  //     $('.search-form-wrapper').toggleClass('open');
+  //     $('.search-form-wrapper .search').focus();
+  //     $('html').toggleClass('search-form-open');
+  //   });
+  //   $('[data-toggle=search-form-close]').click(function() {
+  //     $('.search-form-wrapper').removeClass('open');
+  //     $('html').removeClass('search-form-open');
+  //   });
+  // $('.search-form-wrapper .search').keypress(function( event ) {
+  //   if($(this).val() == "Search") $(this).val("");
+  // });
+
+  // $('.search-close').click(function(event) {
+  //   $('.search-form-wrapper').removeClass('open');
+  //   $('html').removeClass('search-form-open');
+  // });
+  // });
+
+
+
+
+//   const searchBtn = document.querySelector(".search-btn");
+// const cancelBtn = document.querySelector(".cancel-btn");
+// const searchBox = document.querySelector(".search-box");
+
+// searchBtn.onclick = () => {
+//   searchBox.classList.add("active");
+// }
+
+// cancelBtn.onclick = () => {
+//   searchBox.classList.remove("active");
+// }
