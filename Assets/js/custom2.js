01dc81802f64ee41dcdb6b48df12d8fbefc1bf96
Sarah Nnanyelugo <sarahnnanyelugo@gmail.com>
2022-03-11 20:12:54 +0100

    blog

--- Assets/js/custom2.js
+++ Assets/js/custom2.js
@@ -35,86 +35,42 @@ function includeHTML() {
 
 
 
-	// jQuery(document).ready(function() {
+  var pick=false;
 
-  //   var pick=false;
-  //   $("#searchButton").click(function(e){
-  //     pick=!pick;
-  //     console.log(pick)
-  //     if(pick==true){
-  //       $("#search").addClass("Search-container");
-  //       $("#search").removeClass("hid")
-  //         }
-  //         else{
-  //         $("#search").removeClass("Search-container")
-  //         $("#search").addClass("hid");
-  //         }
-  //   })
 
-  //   $("#closeSearch").click(function(e){
-  //     pick=false;
-  //     $("#searchbar").removeClass("Search-container")
-  //         $("#search").addClass("hid");
+function showSearch(){
+  pick=!pick;
+      console.log(pick)
+      if(pick==true){
+        $("#search").addClass("search-button");
+        $("#searchBtn").addClass("hid")
+        $("#search").removeClass("hid")
+          }
+          else{
+          $("#search").removeClass("search-button")
+          $("#search").addClass("hid");
+          $("#searchBtn").removeClass("hid")
 
-  //   })
-  // });
+          }
+}
 
 
 
+	jQuery(document).ready(function() {
 
+    $("#searchBtn").click(function(e){
 
+    })
 
+    $("#closeSearch").click(function(e){
+      pick=false;
+      $("#search").removeClass("search-container")
+          $("#search").addClass("hid");
 
-  // $( document ).ready(function() {
-  //   $('[data-toggle=search-form]').click(function() {
-  //       $('.search-form-wrapper').toggleClass('open');
-  //       $('.search-form-wrapper .search').focus();
-  //       $('html').toggleClass('search-form-open');
-  //     });
-  //     $('[data-toggle=search-form-close]').click(function() {
-  //       $('.search-form-wrapper').removeClass('open');
-  //       $('html').removeClass('search-form-open');
-  //     });
-  //   $('.search-form-wrapper .search').keypress(function( event ) {
-  //     if($(this).val() == "Search") $(this).val("");
-  //   });
-
-  //   $('.search-close').click(function(event) {
-  //     $('.search-form-wrapper').removeClass('open');
-  //     $('html').removeClass('search-form-open');
-  //   });
-
-
-  //   $('[data-toggle=search-form]').click(function() {
-  //     $('.search-form-wrapper').toggleClass('open');
-  //     $('.search-form-wrapper .search').focus();
-  //     $('html').toggleClass('search-form-open');
-  //   });
-  //   $('[data-toggle=search-form-close]').click(function() {
-  //     $('.search-form-wrapper').removeClass('open');
-  //     $('html').removeClass('search-form-open');
-  //   });
-  // $('.search-form-wrapper .search').keypress(function( event ) {
-  //   if($(this).val() == "Search") $(this).val("");
-  // });
-
-  // $('.search-close').click(function(event) {
-  //   $('.search-form-wrapper').removeClass('open');
-  //   $('html').removeClass('search-form-open');
-  // });
-  // });
+    })
+  });
 
 
 
 
-//   const searchBtn = document.querySelector(".search-btn");
-// const cancelBtn = document.querySelector(".cancel-btn");
-// const searchBox = document.querySelector(".search-box");
 
-// searchBtn.onclick = () => {
-//   searchBox.classList.add("active");
-// }
-
-// cancelBtn.onclick = () => {
-//   searchBox.classList.remove("active");
-// }
